df8945c47e01bf92f500d9ab17feb841c1a3b86d
vonmarasigan <von.marasigan@ft.com>
2025-02-05 01:12:28 +0000

    Update app.py

--- app.py
+++ app.py
@@ -3,12 +3,12 @@ from flask import Flask
 app=Flask(__name__)
 
 @app.route('/')
-def greetme():
-    return 'Hello Deveops'
+def greeting():
+    return 'Hello, World!'
 
 @app.route('/page1')
 def page1():
-    return 'welcome to page1'
+    return 'This is page 1'
 
-if  __name__ == '__main__':
-    app.run(host='0.0.0.0',port=5000)
+if __name__=='__main__':
+    app.run(host="0.0.0.0", port=5000)
