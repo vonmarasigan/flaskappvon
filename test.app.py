10edfe88b656964402502d58a9e2675561e659fc
vonmarasigan <von.marasigan@ft.com>
2025-02-05 01:09:14 +0000

    Update test.app.py

--- test.app.py
+++ test.app.py
@@ -7,13 +7,13 @@ def client():
     with app.test_client() as client:
         yield client
 
-def test_greetime(client):
+def test_greetme(client):
     response = client('/')
     assert response.data==b'Hello Deveops'
     assert response.status==200
 
 
-def test_page(client):
+def test_page1(client):
     response = client('/page1')
     assert response.data==b'welcome to page1'
     assert response.status==200
