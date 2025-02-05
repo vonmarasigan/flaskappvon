77bf82be4f9744209348ea2fba8534dc3294d613
vonmarasigan <von.marasigan@ft.com>
2025-02-05 01:10:49 +0000

    Update app.py

--- app.py
+++ app.py
@@ -4,11 +4,11 @@ app=Flask(__name__)
 
 @app.route('/')
 def greetme():
-    return "Hello Deveops"
+    return 'Hello Deveops'
 
 @app.route('/page1')
 def page1():
-    return "welcome to page1"
+    return 'welcome to page1'
 
 if  __name__ == '__main__':
     app.run(host='0.0.0.0',port=5000)
