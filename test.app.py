ad17560ca08d46b5e51bcc5f8b7e25a41208b5e8
vonmarasigan <von.marasigan@ft.com>
2025-02-05 01:12:48 +0000

    Update test.app.py

--- test.app.py
+++ test.app.py
@@ -1,4 +1,3 @@
-from urllib import response
 import pytest
 from app import app
 
@@ -7,13 +6,12 @@ def client():
     with app.test_client() as client:
         yield client
 
-def test_greetme(client):
-    response = client('/')
-    assert response.data==b'Hello Deveops'
-    assert response.status==200
-
+def test_greetings(client):
+    response = client.get('/')
+    assert response.data == b'Hello, World!'
+    assert response.status_code == 200
 
 def test_page1(client):
-    response = client('/page1')
-    assert response.data==b'welcome to page1'
-    assert response.status==200
+    response = client.get('/page1')
+    assert response.data == b'This is page 1'
+    assert response.status_code == 200
